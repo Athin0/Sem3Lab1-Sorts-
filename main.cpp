e3bb8cc05bdfd013ba99a994cc7f1efc9549532a
Arina <Athin0>
2021-07-08 13:30:32 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include "vector"
+#include "random"
 using namespace std;
+
+
+template <class T>
+ostream& operator<< (ostream &cout, const vector<T> &a){
+    cout<< "{";
+    for (auto i: a){
+        cout<<i ;
+    if ( i != a.back())
+        cout<< ", ";
+    }
+    cout<< "}";
+}
+
 int main() {
     vector <int> a;
     a = {1,2,3};
     a.push_back(89);
-    //cout<< a;
-    for(auto i: a)
-        cout<<i << endl;
+    cout<< a;
     return 0;
 }
