89521a549d3833733f2d060fd628b8dcf01666f3
Arina <Athin0>
2021-07-08 13:15:32 +0300

    Begin 08/07/21

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+#include "vector"
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector <int> a;
+    a = {1,2,3};
+    a.push_back(89);
+    //cout<< a;
+    for(auto i: a)
+        cout<<i << endl;
     return 0;
 }
