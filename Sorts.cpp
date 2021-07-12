8bbb65775d3c52aaa0c2c466c1a42146e463fe2b
Arina <Athin0>
2021-07-13 01:43:19 +0300

    10 CoctailSort QuickSort Хаори (не переделалась)

--- Sorts.cpp
+++ Sorts.cpp
@@ -4,85 +4,108 @@
 
 #include "Sorts.h"
 #include "DynamicArraySequence.h"
-template <class T>
-void swap(Sequence<T>& seq, int i1, int i2) {
+
+template<class T>
+void swap(Sequence<T> &seq, int i1, int i2) {
     T item = seq.Get(i1);
-    seq.Set( i1,seq.Get(i2));
-    seq.Set(i2,item);
+    seq.Set(i1, seq.Get(i2));
+    seq.Set(i2, item);
 }
 
-template <class T>
-void sortBubble(Sequence<T>& vec) {
+template<class T>
+void sortBubble(Sequence<T> &vec) {
     for (int i = 0; i < vec.GetLength(); i++)
-        for (int j = 0; j < vec.GetLength() - 1; j++) {
-            if (vec.Get(j) > vec.Get(j + 1) ){
-                swap(vec,j,j+1);
+        for (int j = 0; j < vec.GetLength() - i - 1; j++) {
+            if (vec.Get(j) > vec.Get(j + 1)) {
+                swap(vec, j, j + 1);
             }
         }
 }
-template <class T>
-void sortInsertion(Sequence<T>& vec){
-    for(int i=1;i<vec.GetLength();i++){
-        for (int j=i;j>0 && vec.Get(j-1)>vec.Get(j);j--){
-            swap(vec,j,j-1);
+
+template<class T>
+void sortCocktail(Sequence<T> &vec) {
+    int begin = 0;
+    int end = vec.GetLength() - 1;
+    for (int i = 0; i < vec.GetLength() / 2; i++) {
+        for (int j = begin; j < end; j++) {
+            if (vec.Get(j) > vec.Get(j + 1)) {
+                swap(vec, j, j + 1);
+            }
+        }
+        for (int j = end; j > begin; j--) {
+            if (vec.Get(j - 1) > vec.Get(j)) {
+                swap(vec, j, j - 1);
+            }
+        }
+        begin++;
+        end--;
+    }
+}
+
+
+template<class T>
+void sortInsertion(Sequence<T> &vec) {
+    for (int i = 1; i < vec.GetLength(); i++) {
+        for (int j = i; j > 0 && vec.Get(j - 1) > vec.Get(j); j--) {
+            swap(vec, j, j - 1);
         }
     }
 }
 
-template <class T>
-void sortSelection(Sequence<T>& vec){
-    for( int i=0;i<vec.GetLength()-1;i++){
-        int min_ind= i;
-        for (int j=i+1;j<vec.GetLength();j++){
-            if (vec[min_ind]>vec.Get(j))
-                min_ind= j;
+template<class T>
+void sortSelection(Sequence<T> &vec) {
+    for (int i = 0; i < vec.GetLength() - 1; i++) {
+        int min_ind = i;
+        for (int j = i + 1; j < vec.GetLength(); j++) {
+            if (vec[min_ind] > vec.Get(j))
+                min_ind = j;
         }
-        swap(vec,min_ind,i);
+        swap(vec, min_ind, i);
     }
 }
 
 
-template <class T>
-void sortMerge(Sequence<T> &array,int first, int last) {
-    if (first<last-1) {
+template<class T>
+void sortMerge(Sequence<T> &array, int first, int last) {
+    if (first < last - 1) {
         sortMerge(array, first, (first + last) / 2);
-        sortMerge(array, (first + last) / 2 , last);
+        sortMerge(array, (first + last) / 2, last);
         Merge(array, first, last);
     }
 }
 
-template <class T>
-void Merge(Sequence<T> &array,int begin, int last){
+template<class T>
+void Merge(Sequence<T> &array, int begin, int last) {
     ArraySequence<T> temp_array;
-    int mid, first,second;
-    mid=(begin+last)/2;
-    first= begin;
-    second=mid;
-    for (int i=begin;i<last;i++){
-        if(first<mid && (second>=last|| array.Get(first)<array.Get(second))){
+    int mid, first, second;
+    mid = (begin + last) / 2;
+    first = begin;
+    second = mid;
+    for (int i = begin; i < last; i++) {
+        if (first < mid && (second >= last || array.Get(first) < array.Get(second))) {
             temp_array.Append(array.Get(first));
             first++;
-        } else{
+        } else {
             temp_array.Append(array.Get(second));
             second++;
         }
     }
-    for (int j=begin; j<last; j++) array.Set(j,temp_array.Get(j-begin));
+    for (int j = begin; j < last; j++) array.Set(j, temp_array.Get(j - begin));
 }
 
 
-template <class T>
-int partition(Sequence<T>& A, int low,int high){
-    int rand=(low+high)/2;
+template<class T>
+int partition(Sequence<T> &A, int low, int high) {
+    int rand = (low + high) / 2;
     T pivot = A[high];    //никакой не рандом!!!!
     int temp = low;
-    for(int i =low;i<high;i++){
-        if (A[i]<pivot){
-            swap(A,i,temp);
+    for (int i = low; i < high; i++) {
+        if (A[i] < pivot) {
+            swap(A, i, temp);
             temp++;
         }
     }
-        swap(A,temp,high);
+    swap(A, temp, high);
     /*
     cout<<pivot<<":::: ";
     for(int i =low;i<high;i++){
@@ -93,38 +116,40 @@ int partition(Sequence<T>& A, int low,int high){
     return temp;
 
 };
-template <class T>
-void sortQuick(Sequence<T>& vec, int low,int high){
-    if(high>low) {
+
+template<class T>
+void sortQuick(Sequence<T> &vec, int low, int high) {
+    if (high > low) {
         int p = partition(vec, low, high);
-        sortQuick(vec, low, p-1);
-        sortQuick(vec, p+1, high);
+        sortQuick(vec, low, p - 1);
+        sortQuick(vec, p + 1, high);
     }
 }
 
 
-template <class T>
-int partitionHaore(Sequence<T>& A, int low,int high){
-    T pivot = (A[low]+A[high])/2;
+template<class T>
+int partitionHaore(Sequence<T> &A, int low, int high) {
+    T pivot = (A[low] + A[high - 1]) / 2;
     int begin = low;
-    int end=high;
-    while (true){
-        while (A[begin]<pivot){
+    int end = high - 1;
+    while (true) {
+        while (A[begin] < pivot) {
             begin++;
         }
-        while (A[end]>pivot){
+        while (A[end] > pivot) {
             end--;
         }
-        if(begin>=end)
-         return end;
-        swap(A,begin++,end--);
+        if (begin >= end)
+            return end;
+        swap(A, begin++, end--);
     }
 }
+
 template<class T>
-void sortQuickHoare(Sequence<T>& vec, int low,int high){
-    if(high>low) {
+void sortQuickHoare(Sequence<T> &vec, int low, int high) {
+    if (high > low) {
         int p = partitionHaore(vec, low, high);
         sortQuick(vec, low, p);
-        sortQuick(vec, p+1, high);
+        sortQuick(vec, p, high);
     }
 }
--- main.cpp
+++ main.cpp
@@ -7,14 +7,18 @@
 using namespace std;
 
 int main() {
-    int n=15;
+    int n=16;
     ArraySequence<int> a;
     for (int i = 0; i < n; i++) {
        a.Append(rand()%100);
     }
-    cout << a<<endl;
-    sortQuickHoare(a,0,n-1);
+    ArraySequence<int> b(a);
+    //cout << a<<endl;
+    sortBubble(b);
+    sortCocktail(a);
     cout<< a;
-
+    for (int i = 0; i < n; i++) {
+        if(a[i]!=b[i]) cout<< "блять";
+    }
     return 0;
 }
